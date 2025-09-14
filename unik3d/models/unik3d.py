7d4a47f277866209984c72bf2de9b61e895039c0
sakaridis <csakarid@ethz.ch>
2025-09-15 01:02:36 +0200

    Fix inference - rectify issue with paddings (#34)
    Co-authored-by: Christos Sakaridis <csakarid@eu-g6-080.euler.ethz.ch>

--- unik3d/models/unik3d.py
+++ unik3d/models/unik3d.py
@@ -46,16 +46,20 @@ def get_paddings(original_shape, aspect_ratio_range):
 
     if orig_aspect_ratio > target_aspect_ratio:  # Too wide
         W_new = W_ori
-        H_new = int(W_ori / target_aspect_ratio)
+        H_new = round(W_ori / target_aspect_ratio)
         pad_top = (H_new - H_ori) // 2
         pad_bottom = H_new - H_ori - pad_top
         pad_left, pad_right = 0, 0
-    else:  # Too tall
+    elif orig_aspect_ratio < target_aspect_ratio:  # Too tall
         H_new = H_ori
-        W_new = int(H_ori * target_aspect_ratio)
+        W_new = round(H_ori * target_aspect_ratio)
         pad_left = (W_new - W_ori) // 2
         pad_right = W_new - W_ori - pad_left
         pad_top, pad_bottom = 0, 0
+    else:  # Aspect ratio OK
+        H_new = H_ori
+        W_new = W_ori
+        pad_left, pad_right, pad_top, pad_bottom = 0, 0, 0, 0
 
     return (pad_left, pad_right, pad_top, pad_bottom), (H_new, W_new)
 
@@ -71,8 +75,8 @@ def get_resize_factor(original_shape, pixels_range, shape_multiplier=14):
 
     # Calculate the resize factor
     resize_factor = (target_pixels / n_pixels_ori) ** 0.5
-    new_width = int(W_ori * resize_factor)
-    new_height = int(H_ori * resize_factor)
+    new_width = round(W_ori * resize_factor)
+    new_height = round(H_ori * resize_factor)
     new_height = ceil(new_height / shape_multiplier) * shape_multiplier
     new_width = ceil(new_width / shape_multiplier) * shape_multiplier
 
